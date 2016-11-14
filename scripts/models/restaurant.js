638fc90112f314095ccfa2fc1055aff59b4fd1a7
Ted Callahan <CCallahanIV@gmail.com>
2016-11-14 12:08:32 -0800

    Created restaurant.js and restaurantView.js files

--- scripts/models/restaurant.js
+++ scripts/models/restaurant.js
@@ -3,9 +3,9 @@
 (function(module) {
   var restaurant = {}
   restaurant.allRestaurants = [];
-  restaurant.queryParams = {term: 'restaurants', location: 'seattle'};
+  restaurant.queryParams = {term: 'restaurants', location: geoLocation.formattedAddress};
 
-  restaurant.requestRestaurant = function(callback){
+  restaurant.requestRestaurants = function(callback){
     $.get('/requestYelpRestaurants', restaurant.queryParams, function(data){
       restaurant.allRestaurants = data.businesses;
       console.log(restaurant.allRestaurants);
