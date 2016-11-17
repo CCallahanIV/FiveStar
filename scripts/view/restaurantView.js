461ddcf84037f1adcd5c84545fcad98b404e4151
AndShum <ashumway2@gmail.com>
2016-11-17 11:25:11 -0800

    details section is now correctly responive

--- scripts/view/restaurantView.js
+++ scripts/view/restaurantView.js
@@ -35,12 +35,13 @@
 
   restaurantView.handleListDetails = function() {
     $('.lists').on('click', '#showMore', function() {
-      $(this).parent().next().slideToggle('slow');
+      $(this).parent().next().addClass('openRestaurant').slideToggle('slow');
       $(this).parent().parent().siblings().fadeToggle();
       if($(this).text() === 'Show More'){
         $(this).text('Show Less');
       } else {
         $(this).text('Show More');
+        // $(this).removeClass('openRestaurant');
       }
     });
   };
@@ -53,13 +54,13 @@
           var restaurantsObjects = JSON.parse(localStorage.getItem('favorites'));
           restaurantsObjects.unshift(obj);
           localStorage.setItem('favorites', JSON.stringify(restaurantsObjects));
-          restaurantView.renderObject([obj], '#favoritesList', '#rest-template')
+          restaurantView.renderObject([obj], '#favoritesList', '#rest-template');
         } else {
           var restObj = JSON.stringify([obj]);
-          localStorage.setItem('favorites', restObj)
+          localStorage.setItem('favorites', restObj);
         }
       }
-    })
+    });
   });
 
   restaurantView.handleListTeaser();
